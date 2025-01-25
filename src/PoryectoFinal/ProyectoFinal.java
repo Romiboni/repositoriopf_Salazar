ca36bd6ecf5221038af62750c5150fbd13988fca
asistentecontable <asistentecontable@asistente>
2025-01-24 19:36:18 -0500

    Inicio de proyecto

---
+++ src/PoryectoFinal/ProyectoFinal.java
@@ -0,0 +1,35 @@
+package PoryectoFinal;
+
+import java.util.Scanner;
+
+public class ProyectoFinal {
+    public static void main(String[] args) {
+    // Declarar variables
+    int vt1,vt2,vt3;
+    double sm,pt,c;
+    String vendedor; 
+    // Crear objeto de lectura de datos
+    Scanner lectura=new Scanner(System.in);
+    // Entrada de datos
+    System.out.print("Nombre de vendedor: ");
+    vendedor = lectura.next();  // Leer el nombre
+    System.out.print("Sueldo mensual: ");
+    sm=lectura.nextDouble();
+    System.out.print("Venta 1: ");
+    vt1 = lectura.nextInt();
+    System.out.print("Venta 2: ");
+    vt2 = lectura.nextInt();
+    System.out.print("Venta 3: ");
+    vt3 = lectura.nextInt();
+    // Proceso de datos
+    c=(vt1 + vt2 + vt3) * 0.10;
+    pt=sm+c;
+    // Salida de datos
+    System.out.print("La comisión de venta es: " + c);
+    System.out.print("El sueldo neto del vendedor "+vendedor+ " es: " + pt);
+    }    
+}
+
+    
+    
+
